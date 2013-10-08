c39bcff3c67095c3e8e5248bfb85e51dcbb94b36
grant <grant@grant-VirtualBox.(none)>
2013-10-07 18:59:45 -0700

    Start of server class and spec

--- queue/spec/server_spec.rb
+++ queue/spec/server_spec.rb
@@ -1,9 +1,13 @@
-# server_spec.rb
+require './app/server.rb'
 
-# require './app/server.rb'
+describe "When you create a server object" do
+  it "should be record the address" do
+    server = Server.new("localhost", 2345)
+    server.address.should eq("localhost")
+  end
 
-describe "Do something in future" do
-  it "should be useful" do
-    "hello world".should eq("blah")
-  end 
+  it "should record the port" do
+    server = Server.new("localhost", 2345)
+    server.port.should eq(2345)
+  end
 end
